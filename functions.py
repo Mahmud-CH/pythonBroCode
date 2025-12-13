78232c7915a8a3803627a9fffbc791367dffa4fc
MahmudCh <54221296+Mahmud-CH@users.noreply.github.com>
2025-12-13 15:25:32 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,31 @@
+# functions in python: you just type def functionName(): functionBody 
+#                      thats it atleast for the functions that don't 
+#                      take any parameters.
+#                      You can't use overloaded functions
+#                  ex: def greeting():     def greeting(name): doesn't work
+#                      returns and early return and all that works the same way
+#                      they do in cpp 
+
+
+def greeting():
+  print("Hello there.")
+
+def greetingWithNameParameter(name):
+  print(f"Hello {name}")
+
+
+def add(x, y):
+  z = x + y
+  return z
+
+
+def capitalizeFirstLetter(sentence):
+  sentence = sentence.capitalize()
+  return sentence
+
+greeting()
+greetingWithNameParameter("Mahmud")
+
+print(add(1, 2))
+
+print( capitalizeFirstLetter("mahmud") )
